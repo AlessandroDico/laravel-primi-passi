8402220081e0c93a22ebbad607f9e904a4e48c03
Ale <alessandrochb@gmail.com>
2020-12-21 16:27:47 +0100

    creata e collegata prima homepage grezza + scss

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <h1>home page</h1>
+    </body>
+</html>
